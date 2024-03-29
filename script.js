253abf032747a1eac9464f0634cbaabeef7383f3
Sudhansh702 <snonu522@gmail.com>
2024-03-29 14:03:06 +0530

    not working scroll error

--- script.js
+++ script.js
@@ -1,47 +1,73 @@
 let a = 0;
 let o = null;
-let b = 0;
-let r = 0;
-var result = document.querySelector("#result")
-function cal(inp){
-    if(inp==='='){
-        if(typeof b == 'number' && typeof a == 'number' && typeof o == 'string'){
-            result.innerHTML = calulate(a,o,b)
-            a=result.innerHTML 
-            b=null
-            o=null
-        }
+let b = null;
+let result = document.querySelector("#result");
+result.onclick(() => {
+    result.innerHTML = "hllo"
+})
+function appendNumber(number) {
+    if (typeof o == 'string') {
+        b = b * 10 + number;
+        result.innerHTML = a + o + b;
+    } else {
+        a = a * 10 + number;
+        result.innerHTML = a;
     }
-    else if (typeof inp=="number") {
-        if(typeof o == 'string'){
-            b= b*10 +inp
-            result.innerHTML = a + o + b
-        }else{
-            a = a*10 + inp
-            result.innerHTML = a
-        }
-        
+}
+
+function appendOperator(op) {
+    if (typeof o == 'string') {
+        a = calculateResult();
+        b = null
     }
-    else if(typeof inp == "string" ){
-        o = inp
-        result.innerHTML = a + o
+    o = op;
+    result.innerHTML = a + o;
+}
+
+function calculateResult() {
+    if (o == null) return;
+
+    switch (o) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
 
     }
 }
-function calulate(a,o,b){
-    if(o=='+'){
-        return a+b
-    }else if(o=='-'){
-        return a-b
-    }else if(o=='*'){
-        return a*b
-    }else if(o=='/'){
-        return a/b
-    }
+
+function reset() {
+    result.innerHTML = 0;
+    a = 0;
+    o = null;
+    b = 0;
 }
-function reset(){
-    result.innerHTML = 0
-    a=0
-    o=null
-    b=0
+
+function backspace() {
+    if (typeof a == 'number' && a !== 0) {
+        a = a.toString().slice(0, -1);
+        if (a.length === 0) {
+            a = 0;
+        }
+        a = parseFloat(a);
+        result.innerHTML = a;
+    } else if (typeof b == 'number' && b !== 0) {
+        b = b.toString().slice(0, -1);
+        if (b.length === 0) {
+            b = 0;
+        }
+        b = parseFloat(b);
+        result.innerHTML = a + o;
+    } else if (typeof o == 'string') {
+        a = a.toString().slice(0, -1);
+        if (a.length === 0) {
+            a = 0;
+        }
+        a = parseFloat(a);
+        result.innerHTML = a;
+    }
 }
