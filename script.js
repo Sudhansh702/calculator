12fa9bd3548ce03d3846405d7780234a8d33f934
Sudhansh702 <snonu522@gmail.com>
2024-05-03 13:34:39 +0530

    project done , fully working

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+let display = document.getElementById("display")
 
-let res = document.querySelector('#result')
-function appendNumber(a){
-    console.log(a)
-    res.innerHTML = a
-}
+function appendOp(o) {
+    if (display.value.length == 0) {
+        return
+    }else{
+        let lc = display.value.slice(-1)
+        if (/[0-9]/.test(lc)) {
+            display.value += o
+        }else{
+            display.value = display.value.slice(0,-1) + o
+        }
+    }
+} 
